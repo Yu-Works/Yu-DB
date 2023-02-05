3680f0a7420ba8423d43620d42889f99346aab69
IceCream <www@withdata.net>
2023-02-06 02:57:08 +0800

    重构 HibernateTransactionImpl。

--- src/main/kotlin/com/icecreamqaq/yudb/jpa/hibernate/HibernateTransactionImpl.kt
+++ src/main/kotlin/com/icecreamqaq/yudb/jpa/hibernate/HibernateTransactionImpl.kt
@@ -3,27 +3,22 @@ package com.icecreamqaq.yudb.jpa.hibernate
 import com.IceCreamQAQ.Yu.annotation.InstanceMode
 import com.IceCreamQAQ.Yu.hook.HookContext
 import com.IceCreamQAQ.Yu.hook.HookInfo
-import com.IceCreamQAQ.Yu.hook.HookMethod
 import com.IceCreamQAQ.Yu.hook.HookRunnable
 import com.icecreamqaq.yudb.jpa.annotation.Transactional
+import org.hibernate.Session
 import org.hibernate.Transaction
 import java.util.concurrent.ConcurrentHashMap
-import javax.inject.Inject
 
 @InstanceMode
 class HibernateTransactionImpl(val context: HibernateContext) : HookRunnable {
 
-    data class YuTran(val methodHash: Int, val transaction: Transaction)
+    data class YuTran(
+        val methodHash: Int,
+        val session: Session,
+        val transaction: Transaction
+    )
 
-    private val transactionMethodMap = ConcurrentHashMap<String, Array<String>>()
-    private val trans: HashMap<String, ThreadLocal<YuTran>> = HashMap(context.emf.size)
-
-
-    init {
-        for (key in context.emf.keys) {
-            trans[key] = ThreadLocal()
-        }
-    }
+    private val trans: ThreadLocal<YuTran> = ThreadLocal()
 
 
     override fun init(info: HookInfo) {
@@ -31,42 +26,43 @@ class HibernateTransactionImpl(val context: HibernateContext) : HookRunnable {
         info.saveInfo["YuDB.dbList"] = dbList
     }
 
-    override fun preRun(method: HookContext): Boolean {
-        val dbList = method.info.saveInfo["YuDB.dbList"] as Array<String>
+    override fun preRun(context: HookContext): Boolean {
+        val dbList = context.info.saveInfo["YuDB.dbList"] as Array<String>
+        val transList = ArrayList<YuTran>(dbList.size)
         for (db in dbList) {
-            val trans = trans[db] ?: error("Can't find DataSource: $db")
             val yut = trans.get()
-            if (yut != null) if (yut.methodHash == method.info.method.hashCode()) error("This Thread In A Transaction! Can't Open A New Transaction!") else return false
-
-            val tran = context.getSession(db).beginTransaction()
-            trans.set(YuTran(method.info.method.hashCode(), tran))
+            if (yut != null) return false
+            val session = this.context.getSession(db)
+            val tran = session.beginTransaction()
+            YuTran(context.info.method.hashCode(), session, tran).let {
+                transList.add(it)
+                trans.set(it)
+            }
         }
-
+        context.saveInfo("YuDB.Trans", transList)
         return false
     }
 
-    override fun postRun(method: HookContext) {
-        val dbList = method.info.saveInfo["YuDB.dbList"] as Array<String>
-        for (db in dbList) {
-            val trans = trans[db] ?: error("Can't find DataSource: $db")
-            val yut = trans.get()
-            if (yut.methodHash != method.info.method.hashCode()) return
-            yut.transaction.commit()
-            context.getSession(db).close()
-            trans.remove()
+    override fun postRun(context: HookContext) {
+        val trans = context.getInfo("YuDB.Trans") as? List<YuTran> ?: return
+        var throwable: Throwable? = null
+        try {
+            trans.forEach { it.transaction.commit() }
+        } catch (e: Throwable) {
+            throwable = e
+            trans.forEach { it.transaction.rollback() }
         }
+        trans.forEach { it.session.close() }
+        this.trans.remove()
+        throwable?.let { throw it }
     }
 
-    override fun onError(method: HookContext): Boolean {
-        val dbList = method.info.saveInfo["YuDB.dbList"] as Array<String>
-        for (db in dbList) {
-            val trans = trans[db] ?: error("Can't find DataSource: $db")
-            val yut = trans.get()
-            if (yut.methodHash != method.info.method.hashCode()) return false
-            yut.transaction.rollback()
-            context.getSession(db).close()
-            trans.remove()
-        }
+
+    override fun onError(context: HookContext): Boolean {
+        val trans = context.getInfo("YuDB.Trans") as? List<YuTran> ?: return false
+        trans.forEach { it.transaction.rollback() }
+        trans.forEach { it.session.close() }
+        this.trans.remove()
         return false
     }
 }
